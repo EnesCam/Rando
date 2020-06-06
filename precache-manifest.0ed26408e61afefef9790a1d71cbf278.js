bba8441ff3df2e0932221ec972b0738978f7548e
EnesCam <66518381+EnesCam@users.noreply.github.com>
2020-06-06 15:18:45 +0300

    new

---
+++ precache-manifest.0ed26408e61afefef9790a1d71cbf278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37c8c45f485f126e39b1fd6dd260748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972736e38e62b6c9e51f",
+    "url": "/static/css/main.d6a16f22.chunk.css"
+  },
+  {
+    "revision": "ef647e19fe76268bcb7f",
+    "url": "/static/js/2.c822b841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c822b841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972736e38e62b6c9e51f",
+    "url": "/static/js/main.13e08ed6.chunk.js"
+  },
+  {
+    "revision": "70d58d04540e6db6eb0f",
+    "url": "/static/js/runtime-main.41ab5c2a.js"
+  }
+]);
